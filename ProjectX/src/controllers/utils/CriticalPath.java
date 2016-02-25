4783ab420394636fe6b98e01d123cd0ed3ff9c8d
VictorDanielPopescu <v.popescu2@campus.unimib.it>
2016-02-25 19:13:28 +0100

    Did little fixes to CriticalPath and CriticalPathMethodTest

--- ProjectX/src/controllers/utils/CriticalPath.java
+++ ProjectX/src/controllers/utils/CriticalPath.java
@@ -1,25 +1,21 @@
 package controllers.utils;
 
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import models.StageBean;
-
 
 public class CriticalPath {
 
 	private CriticalPath() {
 
 	}
-
 
 	private static DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -29,73 +25,58 @@ public class CriticalPath {
 
 	public static List<StageBean> computeCriticalStages(Map<StageBean, List<StageBean>> mapPrecedences) {
 		setLastCritical(mapPrecedences);
-		//Iterator<Map.Entry<Integer, List<StageBean>>> mapIterator;
 		while (!mapPrecedences.isEmpty()) {
-			//mapIterator = mapPrecedences.entrySet().iterator();
 			List<StageBean> toRemove = new ArrayList<StageBean>();
-			for(Map.Entry<StageBean, List<StageBean>> pair : mapPrecedences.entrySet()) {
-				//Map.Entry<Integer, List<StageBean>> pair = (Map.Entry<Integer, List<StageBean>>) mapIterator.next();
+			for (Map.Entry<StageBean, List<StageBean>> pair : mapPrecedences.entrySet()) {
 				System.out.println(pair);
 				if (pair.getValue() == null) {
 					System.out.println("non ho precedenti");
 					mapES.put(pair.getKey().getIdStage(), 0);
 					toRemove.add(pair.getKey());
-					//mapPrecedences.remove(pair.getKey());
-					//mapIterator.remove();
 				} else if (precedencesHasES(pair)) {
-						System.out.println("ho precedenti con ES");
-						try {
-							int ES = (int)computeES(pair.getValue());
-							mapES.put(pair.getKey().getIdStage(), (Integer) ES);
-							toRemove.add(pair.getKey());
-
-							//mapPrecedences.remove(pair.getKey());
-							//mapIterator.remove();
-
-						} catch (ParseException e) {
-							// TODO handle with a logger
-						}
+					System.out.println("ho precedenti con ES");
+					try {
+						int ES = (int) computeES(pair.getValue());
+						mapES.put(pair.getKey().getIdStage(), (Integer) ES);
+						toRemove.add(pair.getKey());
+					} catch (ParseException e) {
+						// TODO handle with a logger
 					}
+				}
 			}
-			
-			
-			for(StageBean removable : toRemove){
+			for (StageBean removable : toRemove) {
 				System.out.println("rimouovo gli elementi che hanno ES");
 				System.out.println(removable);
 				mapPrecedences.remove(removable);
 			}
 		}
-		for(Map.Entry<Integer,Integer> p : mapES.entrySet()){
+		for (Map.Entry<Integer, Integer> p : mapES.entrySet()) {
 			System.out.println("mapES " + p.getValue());
 		}
 		return criticalTasks;
 	}
 
-	public static void setLastCritical(Map<StageBean, List<StageBean>> mapPrecedences){
-		for(Map.Entry<StageBean, List<StageBean>> pairKey : mapPrecedences.entrySet()){
+	public static void setLastCritical(Map<StageBean, List<StageBean>> mapPrecedences) {
+		for (Map.Entry<StageBean, List<StageBean>> pairKey : mapPrecedences.entrySet()) {
 			boolean b = true;
-			for(Map.Entry<StageBean, List<StageBean>> pairValue : mapPrecedences.entrySet()){
-				if(pairValue.getValue() != null && pairValue.getValue().contains(pairKey.getKey())){
+			for (Map.Entry<StageBean, List<StageBean>> pairValue : mapPrecedences.entrySet()) {
+				if (pairValue.getValue() != null && pairValue.getValue().contains(pairKey.getKey())) {
 					b = false;
 				}
 			}
 			System.out.println(b);
-			if(b){
+			if (b) {
 				criticalTasks.add(pairKey.getKey());
 			}
 		}
 	}
-	
+
 	public static boolean precedencesHasES(Map.Entry<StageBean, List<StageBean>> pair) {
-		//System.out.println("sono in precedencesHasES");
 		List<StageBean> precedences = pair.getValue();
-		//Iterator<StageBean> arrayListIterator = precedences.iterator();
 		for (StageBean precedence : precedences) {
 			if (precedence != null && mapES.get(precedence.getIdStage()) == null) {
-				//arrayListIterator.remove();
 				return false;
 			}
-			//arrayListIterator.remove();
 		}
 		return true;
 	}
@@ -113,23 +94,19 @@ public class CriticalPath {
 			long ES = mapES.get(precedence.getIdStage());
 			result = ES + duration;
 			results.add(result);
-			
+
 			System.out.println("result: " + result);
 			System.out.println("duration: " + duration);
-			if (result > max)  {
+			if (result > max) {
 				max = result;
 			}
 		}
-	
-		for(int i = 0; i<results.size(); ++i){
-			if(results.get(i) == max && !criticalTasks.contains(precedences.get(i)) ){
-				
+
+		for (int i = 0; i < results.size(); ++i) {
+			if (results.get(i) == max && !criticalTasks.contains(precedences.get(i))) {
 				criticalTasks.add(precedences.get(i));
 			}
 		}
-		/*if (critical != null) {
-			criticalTasks.add(critical);
-		}*/
 		return max;
 	}
 }
--- ProjectX/src/test/CriticalPathMethodTest.java
+++ ProjectX/src/test/CriticalPathMethodTest.java
@@ -1,11 +1,10 @@
 package test;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -14,117 +13,48 @@ import models.StageBean;
 
 public class CriticalPathMethodTest {
 	@Test
-	public void CriticalPath() throws ParseException{
+	public void CriticalPath() throws ParseException {
 		Map<StageBean, List<StageBean>> map = new HashMap<StageBean, List<StageBean>>();
 		List<StageBean> precedencesA = null;
 		List<StageBean> precedencesB = new ArrayList<StageBean>();
 		List<StageBean> precedencesC = new ArrayList<StageBean>();
 		List<StageBean> precedencesD = new ArrayList<StageBean>();
-		
+
 		StageBean a = new StageBean();
 		StageBean b = new StageBean();
 		StageBean c = new StageBean();
 		StageBean d = new StageBean();
-		
+
 		a.setIdStage(1);
 		b.setIdStage(2);
 		c.setIdStage(3);
 		d.setIdStage(4);
-		
-		
+
 		a.setStartDay("2016-01-01");
 		b.setStartDay("2016-01-06");
 		c.setStartDay("2016-01-07");
 		d.setStartDay("2016-01-12");
-		
+
 		a.setFinishDay("2016-01-05");
 		b.setFinishDay("2016-01-11");
 		c.setFinishDay("2016-01-08");
 		d.setFinishDay("2016-01-14");
-		
-		
-		 precedencesB.add(a);
+
+		precedencesB.add(a);
 		precedencesC.add(a);
 		precedencesD.add(b);
 		precedencesD.add(c);
-		
+
 		map.put(a, precedencesA);
 		map.put(b, precedencesB);
 		map.put(c, precedencesC);
 		map.put(d, precedencesD);
-		
-		
+
 		List<StageBean> criticalStages = CriticalPath.computeCriticalStages(map);
-		
 
-		
-		for(StageBean critical : criticalStages){
-			
+		for (StageBean critical : criticalStages) {
+
 			System.out.println("critico " + critical.getIdStage());
-			
-		
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		Map<Integer, Integer> mapES = new HashMap<Integer, Integer>();
-//		List<StageBean> criticalTasks = new ArrayList<StageBean>();
-//		Iterator<Map.Entry<Integer, List<StageBean>>> mapIterator = mapProva.entrySet().iterator();
-//
-//		Map.Entry<Integer, List<StageBean>> pair = (Map.Entry<Integer, List<StageBean>>) mapIterator.next();
-		
-		
-		 
-		
-		/*StageBean stage1 = new StageBean();
-		stage1.setIdStage(1);
-		stage1.setStartDay("2016-01-01");
-		stage1.setFinishDay("2016-01-31");
-		
-		StageBean stage2 = new StageBean();
-		stage2.setIdStage(2);
-		stage2.setStartDay("2016-01-01");
-		stage2.setFinishDay("2016-01-10");
-		
-		StageBean stage3 = new StageBean();
-		stage3.setIdStage(3);
-		stage3.setStartDay("2016-02-01");
-		stage3.setFinishDay("2016-02-28");*/
-		
-		
-	//	mapProva.put(2, stages);
-	//	mapProva.put(3, stages);
-		
-	/*	stages.add(stage1);
-		stages.add(stage2);
-		stages.add(stage3);
-		
-		mapProva.put(1, stages);
-		*/
-		
-		
-
-		//	mapIterator.forEachRemaining(action);
-	//	System.out.println(pair.getKey());
-	//	pair.setValue(stages);
-	//	assert(pair.getValue() == null);
-	//List<StageBean> criticalStages = controllers.utils.CriticalPath.computeCriticalStages(mapProva);
-	//	CriticalPath.getInstance().precedencesHasES(pair);
-	/*	System.out.println(mapProva.get(1));
-		System.out.println("ciao");
-	for(StageBean critical : criticalStages){
-		System.out.println(critical);
-	}*/
 	}
 }
-	
