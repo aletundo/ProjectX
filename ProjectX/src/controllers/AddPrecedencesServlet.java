eb3966bb479f312427c372b90660ef156eade50a
VictorDanielPopescu <v.popescu2@campus.unimib.it>
2016-02-26 22:12:36 +0100

    Resolved several critical and major issues in AddPrecedencesServlet class and added the possibility to send email if it's needed outsourcing or it's needed a meting

--- ProjectX/src/controllers/AddPrecedencesServlet.java
+++ ProjectX/src/controllers/AddPrecedencesServlet.java
@@ -18,68 +18,77 @@ import controllers.utils.security.SecureProjectStrategy;
 import models.StageBean;
 import models.StageDAO;
 
-
-@WebServlet(name = "AddPrecendencesServlet", urlPatterns = {"/addprecedences"})
+@WebServlet(name = "AddPrecendencesServlet", urlPatterns = { "/addprecedences" })
 public class AddPrecedencesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (!SecureProjectStrategy.getInstance().isAuthorized(request, response, getServletContext()))
-			return;
-		
-		int idProject = Integer.parseInt(request.getParameter("idProject"));
-		List<StageBean> stages = StageDAO.getInstance().getStagesByIdProject(idProject);
-		List<StageBean> stagesQueue = new ArrayList<StageBean>();
-		for(StageBean s : stages){
-			stagesQueue.add(s);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			if (!SecureProjectStrategy.getInstance().isAuthorized(request, response, getServletContext()))
+				return;
+
+			int idProject = Integer.parseInt(request.getParameter("idProject"));
+			List<StageBean> stages = StageDAO.getInstance().getStagesByIdProject(idProject);
+			List<StageBean> stagesQueue = new ArrayList<>();
+			for (StageBean s : stages) {
+				stagesQueue.add(s);
+			}
+			Serializable stagesSer = (Serializable) stages;
+			Serializable stagesQueueSer = (Serializable) stagesQueue;
+
+			HttpSession session = request.getSession();
+			session.setAttribute("stages", stagesSer);
+			session.setAttribute("stagesQueue", stagesQueueSer);
+			request.setAttribute("idProject", idProject);
+
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/add-precedences.jsp");
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			/* TODO LOGGER */
 		}
-		Serializable stagesSer = (Serializable)stages;
-		Serializable stagesQueueSer = (Serializable)stagesQueue;
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("stages", stagesSer);
-		session.setAttribute("stagesQueue", stagesQueueSer);
-		request.setAttribute("idProject", idProject);
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/add-precedences.jsp");
-		dispatcher.forward(request, response);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (!SecureProjectStrategy.getInstance().isAuthorized(request, response, getServletContext()))
-			return;
-		
-		List<StageBean> precedences = new ArrayList<StageBean>();
-		StageBean stage = new StageBean();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			if (!SecureProjectStrategy.getInstance().isAuthorized(request, response, getServletContext()))
+				return;
 
-		List<StageBean> stagesQueue = (List<StageBean>)request.getSession().getAttribute("stagesQueue");
-		
-		stage.setIdStage(stagesQueue.remove(0).getIdStage());
-		
-		if(request.getParameterValues("id-precedences") != null)
-		{
-			String[] idPrecedences = request.getParameterValues("id-precedences");
-			//Build the list with the precedences to store
-			for(String p : idPrecedences){
-				StageBean sb = new StageBean();
-				sb.setIdStage(Integer.parseInt(p));
-				precedences.add(sb);
+			List<StageBean> precedences = new ArrayList<>();
+			StageBean stage = new StageBean();
+
+			List<StageBean> stagesQueue = (List<StageBean>) request.getSession().getAttribute("stagesQueue");
+
+			stage.setIdStage(stagesQueue.remove(0).getIdStage());
+
+			if (request.getParameterValues("id-precedences") != null) {
+				String[] idPrecedences = request.getParameterValues("id-precedences");
+				// Build the list with the precedences to store
+				for (String p : idPrecedences) {
+					StageBean sb = new StageBean();
+					sb.setIdStage(Integer.parseInt(p));
+					precedences.add(sb);
+				}
+				StageDAO.getInstance().addPrecedences(stage, precedences);
 			}
-			StageDAO.getInstance().addPrecedences(stage, precedences);
-		}
-		
-		if(!stagesQueue.isEmpty()){
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/add-precedences.jsp");
-			dispatcher.forward(request, response);
-		}else{
-			CalculateWeights.computeStagesWeight((List<StageBean>)request.getSession().getAttribute("stages"));
-			request.getSession().removeAttribute("stages");
-			request.getSession().removeAttribute("stagesQueue");
-			request.getSession().removeAttribute("idProject");
-			response.sendRedirect(request.getContextPath() + "/project?idProject=" + Integer.parseInt(request.getParameter("idProject")));
+
+			if (!stagesQueue.isEmpty()) {
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/views/add-precedences.jsp");
+				dispatcher.forward(request, response);
+			} else {
+				CalculateWeights.computeStagesWeight((List<StageBean>) request.getSession().getAttribute("stages"));
+				request.getSession().removeAttribute("stages");
+				request.getSession().removeAttribute("stagesQueue");
+				request.getSession().removeAttribute("idProject");
+				response.sendRedirect(request.getContextPath() + "/project?idProject="
+						+ Integer.parseInt(request.getParameter("idProject")));
+			}
+		} catch (Exception e) {
+			/* TODO LOGGER */
 		}
 	}
 }
