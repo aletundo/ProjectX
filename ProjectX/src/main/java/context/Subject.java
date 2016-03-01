593a021886458780dc12596632340a30dd7f2db6
VictorDanielPopescu <v.popescu2@campus.unimib.it>
2016-03-01 20:59:19 +0100

    [WIP] Reworked observer pattern

--- ProjectX/src/main/java/context/Subject.java
+++ ProjectX/src/main/java/context/Subject.java
@@ -6,48 +6,37 @@ import java.util.List;
 import models.StageBean;
 
 public abstract class Subject {
-	
+
 	protected List<Observer> observers;
-	
-	public Subject(){
-		System.out.println("Sono in subject");
+
+	public Subject() {
 		observers = new ArrayList<Observer>();
 	}
-	
-	public Subject(Subject subject){
-		System.out.println("Sono in subject con parametro");
+
+	public Subject(Subject subject) {
 		this.observers = subject.observers;
 	}
-	
-	public void attach(Observer observer){
-		System.out.println("Sono attach");
-		if(observers != null){			
-			System.out.println("Mi sono attaccato");
 
+	public void attach(Observer observer) {
+		if (observers != null) {
 			observers.add(observer);
 		}
 	}
-	public void dettach(Observer observer){
-		System.out.println("Sono attach");
-		if(observer != null){
-			System.out.println("Mi sono staccato");
 
+	public void dettach(Observer observer) {
+		if (observer != null) {
 			observers.remove(observer);
 		}
 	}
-	
-	public void notifyObservers(){
-		System.out.println("Sono in notyfyAll");
-		System.out.println(observers);
 
+	public void notifyObservers() {
 		for (Observer observer : observers) {
-			System.out.println(observer);
-
-			System.out.println("Sono nel for di notyfyAll");
-			System.out.println(observer);
 			observer.update();
 		}
 	}
-	
-	public abstract List<StageBean> getStages();
+
+	public abstract List<StageBean> getStagesNonCritical();
+
+	public abstract List<StageBean> getStagesCritical();
+
 }
--- ProjectX/src/main/java/controllers/utils/SchedulerEventsThread.java
+++ ProjectX/src/main/java/controllers/utils/SchedulerEventsThread.java
@@ -12,18 +12,15 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
-
+import context.Subject;
 import models.StageBean;
-import models.UserDAO;
 
-public class SchedulerEventsThread implements Runnable {
-	
-	public SchedulerEventsThread(int idProject){
+public class SchedulerEventsThread extends Subject implements Runnable {
+
+	public SchedulerEventsThread(int idProject) {
 		this.idProject = idProject;
 	}
-	
+
 	private int idProject;
 
 	public int getIdProject() {
@@ -36,79 +33,83 @@ public class SchedulerEventsThread implements Runnable {
 
 	private static final Logger LOGGER = Logger.getLogger(SchedulerEventsThread.class.getName());
 
-	public static void main(String[] args) {
-		final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		SchedulerEventsThread scheduler = new SchedulerEventsThread(74);
-		service.scheduleAtFixedRate(scheduler, 0, 24, TimeUnit.HOURS);
+	// public static void main(String[] args) {
+	// final ScheduledExecutorService service =
+	// Executors.newSingleThreadScheduledExecutor();
+	// SchedulerEventsThread scheduler = new SchedulerEventsThread(74);
+	// scheduler.attach(new StagesObserver(scheduler));
+	// service.scheduleAtFixedRate(scheduler, 0, 24, TimeUnit.HOURS);
+	// }
+
+	public SchedulerEventsThread() {
+		super();
 	}
 
 	static DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 	String host = "localhost";
-	String port = ""; /* TODO check port */
-	final String pw = null; /* TODO check password */
-	String toAddress = "";
+	String port = "8080"; /* TODO check port */
+	final String pw = "bla"; /* TODO check password */
+	String toAddress = "asd@mail.com";
 
 	@Override
 	public void run() {
-		System.out.println("entro");
-		List<StageBean> stages = new ArrayList<>();
+		List<StageBean> stagesCritical = new ArrayList<>();
+		List<StageBean> stagesNonCritical = new ArrayList<>();
 
-		stages = models.StageDAO.getInstance().getStagesByIdProject(this.getIdProject());
-		System.out.println("ho preso gli stage dal db");
-		long criticalDate;
-		for (StageBean stage : stages) {
+		stagesCritical = getStagesCritical();
+		stagesNonCritical = getStagesNonCritical();
+
+		long criticalDate = 0;
+		for (StageBean stageNonCritical : stagesNonCritical) {
 			try {
-				System.out.println("sono nel try");
-				criticalDate = getDifferenceDays(format.parse(stage.getFinishDay()),
+				criticalDate = getDifferenceDays(format.parse(stageNonCritical.getFinishDay()),
 						format.parse(UtilityFunctions.GetCurrentDateTime()));
-				System.out.println(criticalDate);
-				//non critical stage delay
-				System.out.println(stage.getIdStage());
-				System.out.println("stage is critical? " + stage.getCritical());
-				if (criticalDate < 0 && stage.getRateWorkCompleted() < 100 && "False".equalsIgnoreCase(stage.getCritical())) {
-					System.out.println("RITARDO NON CRITICO");
-					toAddress = models.UserDAO.getInstance().getGenericUserMailById(stage.getIdSupervisor());
-
-					final String userName = stage.getSupervisorFullname();
-					final String subject = "[STAGE DELAY]";
-					final String message = "The stage should have ended but it has not yet done it.";
-
-					controllers.utils.SendEmail.sendEmail(host, port, userName, pw, toAddress, subject, message);
-				} //critical stage delay
-				else if (criticalDate < 0 && stage.getRateWorkCompleted() < 99 && "True".equals(stage.getCritical())) {
-					System.out.println("RITARDO CRITICO");
-					UserDAO.getInstance().getGenericUserMailById(stage.getIdSupervisor());
-
-					final String userName = stage.getSupervisorFullname();
-					final String subject = "[PROJECT DELAY]";
-					final String message = "A critical stage should have ended but it has not yet done it and now the entire project in delaying.";
-
-					String toAddressProj = models.UserDAO.getInstance()
-							.getProjectManagerMailByIdStage(stage.getIdStage());
-
-					controllers.utils.SendEmail.sendEmail(host, port, userName, pw, toAddress, subject, message);
-					controllers.utils.SendEmail.sendEmail(host, port, userName, pw, toAddressProj, subject, message);
-
+				if (criticalDate < 0 && stageNonCritical.getRateWorkCompleted() < 100) {
+					notifyObservers();
 				}
 			} catch (ParseException e) {
 				LOGGER.log(Level.SEVERE, "Something went wrong during parsing a date", e);
-			} catch (AddressException e) {
-				LOGGER.log(Level.SEVERE, "Something went wrong during sending a mail", e);
-			} catch (MessagingException e) {
-				LOGGER.log(Level.SEVERE, "Something went wrong during sending a mail", e);
 			}
 		}
-
+		for (StageBean stageCritical : stagesCritical) {
+			try {
+				criticalDate = getDifferenceDays(format.parse(stageCritical.getFinishDay()),
+						format.parse(UtilityFunctions.GetCurrentDateTime()));
+				if (criticalDate < 0 && stageCritical.getRateWorkCompleted() < 99
+						&& "True".equals(stageCritical.getCritical())) {
+					notifyObservers();
+				}
+			} catch (ParseException e) {
+				LOGGER.log(Level.SEVERE, "Something went wrong during parsing a date", e);
+			}
+		}
 	}
+
 	public static long getDifferenceDays(Date d1, Date d2) {
 		long diff = d1.getTime() - d2.getTime();
-		if(diff < 0){
+		if (diff < 0) {
 			return (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)) - 1;
 		}
-		if(diff > 0){
+		if (diff > 0) {
 			return (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)) + 1;
 		}
 		return (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+	}
+
+	@Override
+	public List<StageBean> getStagesNonCritical() {
+		List<StageBean> stages = new ArrayList<>();
+		/* TODO QUERY */
+		stages = models.StageDAO.getInstance().getStagesByIdProject(this.getIdProject());
+		return stages;
+	}
+
+	@Override
+	public List<StageBean> getStagesCritical() {
+		List<StageBean> stages = new ArrayList<>();
+		/* TODO QUERY */
+		stages = models.StageDAO.getInstance().getStagesByIdProject(this.getIdProject());
+		return stages;
 	}
 }
