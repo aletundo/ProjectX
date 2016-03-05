2b5e96bf60e0aec6e5774951d044efe7d6cb25dd
Alessandro Tundo <a.tundo@campus.unimib.it>
2016-03-05 16:32:36 +0100

    [WIP]Generation of comments in the most important classes

--- ProjectX/src/main/java/controllers/utils/UpdateRateCompleted.java
+++ ProjectX/src/main/java/controllers/utils/UpdateRateCompleted.java
@@ -8,48 +8,67 @@ import models.StageDAO;
 import models.TaskBean;
 import models.TaskDAO;
 
+/**
+ * @author alessandro The aim of the class is to update the percent work
+ *         complete of a project and a stage when one of its tasks is completed.
+ */
 public class UpdateRateCompleted {
 
-    private UpdateRateCompleted() {
+	private UpdateRateCompleted() {
 
-    }
+	}
 
-    public static void checkTaskCompleted(int idTask) {
-        List<String> workCompletedState = TaskDAO.getInstance().checkDevelopersWork(idTask);
-        boolean isTaskCompleted = true;
-        for (String isCompleted : workCompletedState) {
-            if ("False".equals(isCompleted))
-                isTaskCompleted = false;
-        }
+	/**
+	 * @param idTask
+	 *            Check if all developers have completed their work. If it is
+	 *            true, set the task as completed and call
+	 *            updateStageRateCompleted.
+	 */
+	public static void checkTaskCompleted(int idTask) {
+		List<String> workCompletedState = TaskDAO.getInstance().checkDevelopersWork(idTask);
+		boolean isTaskCompleted = true;
+		for (String isCompleted : workCompletedState) {
+			if ("False".equals(isCompleted))
+				isTaskCompleted = false;
+		}
 
-        if (isTaskCompleted) {
-            TaskDAO.getInstance().setTaskCompleted(idTask);
-            updateStageRateCompleted(idTask);
-        }
-    }
+		if (isTaskCompleted) {
+			TaskDAO.getInstance().setTaskCompleted(idTask);
+			updateStageRateCompleted(idTask);
+		}
+	}
 
-    private static void updateStageRateCompleted(int idTask) {
-        TaskBean task = TaskDAO.getInstance().getRelativeWeight(idTask);
-        float rateWorkCompletedToUpdate = StageDAO.getInstance().getRateWorkCompleted(task.getIdStage())
-                + task.getRelativeWeight();
-        if (rateWorkCompletedToUpdate > 99) {
-            rateWorkCompletedToUpdate = 100;
-        }
-        StageDAO.getInstance().setRateWorkCompleted(task.getIdStage(), rateWorkCompletedToUpdate);
-        updateProjectRatecompleted(task.getIdStage());
-    }
+	/**
+	 * @param idTask
+	 *            Update the rate work completed of a stage and call
+	 *            updateProjectRateCompleted.
+	 */
+	private static void updateStageRateCompleted(int idTask) {
+		TaskBean task = TaskDAO.getInstance().getRelativeWeight(idTask);
+		float rateWorkCompletedToUpdate = StageDAO.getInstance().getRateWorkCompleted(task.getIdStage())
+				+ task.getRelativeWeight();
+		if (rateWorkCompletedToUpdate > 99) {
+			rateWorkCompletedToUpdate = 100;
+		}
+		StageDAO.getInstance().setRateWorkCompleted(task.getIdStage(), rateWorkCompletedToUpdate);
+		updateProjectRatecompleted(task.getIdStage());
+	}
 
-    private static void updateProjectRatecompleted(int idStage) {
-        StageBean stage = StageDAO.getInstance().getRelativeWeight(idStage);
-        float stageRateWorkCompleted = StageDAO.getInstance().getRateWorkCompleted(idStage);
-        float rateStageToProjectCompleted = (stageRateWorkCompleted * stage.getRelativeWeight()) / 100;
-        float rateProjectCompletedToUpdate = ProjectDAO.getInstance().getRateWorkCompleted(stage.getIdProject())
-                + rateStageToProjectCompleted;
-        if (rateProjectCompletedToUpdate > 99) {
-            rateProjectCompletedToUpdate = 100;
-        }
-        ProjectDAO.getInstance().setRateWorkCompleted(stage.getIdProject(), rateProjectCompletedToUpdate);
+	/**
+	 * @param idStage
+	 *            Update the rate work completed of a project.
+	 */
+	private static void updateProjectRatecompleted(int idStage) {
+		StageBean stage = StageDAO.getInstance().getRelativeWeight(idStage);
+		float stageRateWorkCompleted = StageDAO.getInstance().getRateWorkCompleted(idStage);
+		float rateStageToProjectCompleted = (stageRateWorkCompleted * stage.getRelativeWeight()) / 100;
+		float rateProjectCompletedToUpdate = ProjectDAO.getInstance().getRateWorkCompleted(stage.getIdProject())
+				+ rateStageToProjectCompleted;
+		if (rateProjectCompletedToUpdate > 99) {
+			rateProjectCompletedToUpdate = 100;
+		}
+		ProjectDAO.getInstance().setRateWorkCompleted(stage.getIdProject(), rateProjectCompletedToUpdate);
 
-    }
+	}
 
 }
